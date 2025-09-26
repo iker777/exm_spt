954dfa41eabe204be3d18deb47d737fe07980a5e
adriancho39 <adriancho10@yandex.com>
2025-09-26 09:49:45 +0200

    primmer commit

---
+++ script-adri.js
@@ -0,0 +1,20 @@
+
+
+// Ejercicio 1
+
+let edad = prompt("¿Cuantos años tienes?")
+
+if (edad >= 0 && edad < 18) {
+    console.log("Eres menor de edad")
+
+} else if (edad >= 18 && edad < 105) {
+    console.log("Eres mayor de edad")
+
+} else console.log("No vale")
+
+// Ejercicio 2
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
+
